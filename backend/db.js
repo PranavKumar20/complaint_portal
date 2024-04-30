b32ba72a1db4432dca8730b401c0fd24a3859261
= <pranavkumar0155@gmail.com>
2024-04-30 12:20:59 +0530

    added db uri

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 // const uri = "mongodb://127.0.0.1:27017/ComplainPortal";
-const uri = "mongodb+srv://pranavkumar0155:<password>@cluster0.l9kq4zp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = "mongodb+srv://pranavkumar0155:cloud0155@cluster0.l9kq4zp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 mongoose.connect(uri);
 
